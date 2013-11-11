ac6a873bdf011602ad8f40b0559a79ab5069bdeb
ghostnotes <ghostnotesdot@gmail.com>
2013-11-12 00:28:44 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ebookfinder::Application.config.secret_key_base = 'd78e954d5c56172a58547d0a5ec9c2c63b5edb508b08d3907dab7b5be66c386cf5ecd7564db82754969787b3efefd108a63446809f2088ebf579cb41cf0a313e'
